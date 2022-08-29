0b857c6cd7bc88017b972c2dd3ab34c6ca2213c6
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-08-29 20:06:48 -0300

    ADD - Algorithm

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define CORREDORES 5
 
 using namespace std;
 
@@ -8,14 +9,46 @@ typedef struct corredor
     string nombre;
 } Corredor;
 
+Corredor ingresoCorredor();
+
 int main()
 {
-    Corredor temp;
-    cout<<"Ingrese el nombre del corredor: ";
-    getline(cin, temp.nombre);
-    cout<<"Ingrese el tiempo del corredor: ";
-    cin>>temp.tiempo;
-    //cout<<temp.nombre<<" corrio durante "<<temp.tiempo<<" minutos."<<endl;
+    Corredor pri;
+    Corredor seg;
+    Corredor ult;
+    float prom = 0;
+    Corredor temp = ingresoCorredor();
+    pri = temp;
+    seg = temp;
+    ult = temp;
+    prom = temp.tiempo;
+    for (int i = 0; i < CORREDORES - 1; i++){
+        temp = ingresoCorredor();
+        if (temp.tiempo < pri.tiempo){
+            seg = pri;
+            pri = temp;
+        } else if (temp.tiempo < seg.tiempo)
+            seg = temp;
+        else if (temp.tiempo > ult.tiempo)
+            ult = temp;
+        prom += temp.tiempo;
+    }
+    prom = prom / CORREDORES;
+
+    cout<<"Primero: "<<pri.nombre<<" ("<<pri.tiempo<<")"<<endl;
+    cout<<"Segundo: "<<seg.nombre<<" ("<<seg.tiempo<<")"<<endl;
+    cout<<"Ultimo: "<<ult.nombre<<" ("<<ult.tiempo<<")"<<endl;
+    cout<<"Promedio: "<<prom<<endl;
 
     return 0;
+}
+
+Corredor ingresoCorredor()
+{
+    Corredor temp;
+    cout<<"Ingrese el tiempo del corredor: ";
+    cin>>temp.tiempo;
+    cout<<"Ingrese el nombre del corredor: ";
+    getline(cin, temp.nombre);
+    return temp;
 }
